7c883c6eecc01e17251a76028bf001c9192164ea
Evi1 <w1zirn1996@gmail.com>
2018-12-28 15:21:10 +0800

    main and test

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package tieba_api
+package main
+
+type rPlugin int
+
+type CallMessage struct {
+	Func    string
+	Cookies string
+}
+
+func (rp rPlugin) RunPlugin(jArgs string) string {
+	return "hello" + jArgs
+}
+
+// exported as symbol named "Greeter"
+var ARPlugin rPlugin
--- test.go
+++ test.go
@@ -1,1 +1,23 @@
-package tieba_api
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type RPlugin interface {
+	RunPlugin(string) string
+}
+
+func main() {
+	var rPlugin RPlugin = ARPlugin
+
+	res := rPlugin.RunPlugin("1")
+	fmt.Print(res)
+
+	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+	var f interface{}
+	json.Unmarshal(b, &f)
+	m := f.(map[string]interface{})
+	fmt.Print(int(m["Age"].(float64)))
+}
