3a46c3c7d286665cf60de0ef861d6ddfbe22a32a
Evi1 <w1zirn1996@gmail.com>
2019-01-24 23:58:12 +0800

    main and test

--- test.go
+++ test.go
@@ -11,14 +11,14 @@ type RPlugin interface {
 
 func main() {
 	var rPlugin RPlugin = ARPlugin
-	m := map[string]interface{}{"func": "check_cookies", "cookies": "BDUSS=0NQdmpwbX4wWmR2MFNBRlEzNVJZeFRkcDFXRGtQLTVzQm92Sy1zZk5teXA5c2xZSVFBQUFBJCQAAAAAAAAAAAEAAACDqdI5utrK~dfWyrHJ0LjnAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKlpolipaaJYZ\n" +
-		"TOKEN=f78835ccc7f13b431103e28415f47f12f398c17e2cf229f730b2b39f4aa9a2aa"}
+	m := map[string]interface{}{"func": "check_cookies", "cookies": "\n" +
+		""}
 	jstr, _ := json.MapToJsonStr(m)
 	fmt.Println(jstr)
 	res := rPlugin.RunPlugin(jstr)
 	fmt.Println(res)
-	m = map[string]interface{}{"func": "get_liked", "cookies": "BDUSS=0NQdmpwbX4wWmR2MFNBRlEzNVJZeFRkcDFXRGtQLTVzQm92Sy1zZk5teXA5c2xZSVFBQUFBJCQAAAAAAAAAAAEAAACDqdI5utrK~dfWyrHJ0LjnAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKlpolipaaJYZ\n" +
-		"TOKEN=f78835ccc7f13b431103e28415f47f12f398c17e2cf229f730b2b39f4aa9a2aa"}
+	m = map[string]interface{}{"func": "get_liked", "cookies": "\n" +
+		""}
 	jstr, _ = json.MapToJsonStr(m)
 	fmt.Println(jstr)
 	res = rPlugin.RunPlugin(jstr)
